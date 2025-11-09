2cc3d395455f507e568a003e80da8839042934e6
Gaurav Mitra <gmitra6@gatech.edu>
2025-11-09 16:39:07 -0500

    added logs/writers for tensorboard

--- train_model.py
+++ train_model.py
@@ -1,12 +1,16 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # 0 = all, 1 = info, 2 = warning, 3 = error
 
+import datetime
 import tensorflow as tf
 from dataLoader import get_data
 from metrics import mse, ade, masked_fde
 
+
 # import models
 from models.ConvMLP import ConvMLP
+
+LOG_DIR = "logs"
 
 MODELS = [
     ConvMLP
@@ -38,10 +42,18 @@ def train_model(model_to_train, BATCH_SIZE=64, epochs=10):
     model = model_to_train(PAST_STEPS, FUTURE_STEPS)
     model.build(input_shape=(None, PAST_STEPS, 2))
 
+    #Initialize for TensorBoard
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = os.path.join(LOG_DIR, current_time, "train")
+    val_log_dir = os.path.join(LOG_DIR, current_time, "val")
+
+    train_writer = tf.summary.create_writer(train_log_dir)
+    val_writer = tf.summary.create_writer(val_log_dir)
+
     optimizer = tf.keras.optimizers.Adam(1e-3)
 
     for epoch in range(epochs):
-        epoch_loss, epoch_ade, epoch_fde = 0.0
+        epoch_loss, epoch_ade, epoch_fde = 0.0, 0.0, 0.0
         num_batches = 0
 
         for past, future, valid in train_ds:
@@ -58,11 +70,17 @@ def train_model(model_to_train, BATCH_SIZE=64, epochs=10):
         epoch_ade  /= num_batches
         epoch_fde  /= num_batches
 
+        # write train metrics
+        with train_writer.as_default():
+            tf.summary.scalar("loss", epoch_loss, step=epoch)
+            tf.summary.scalar("ADE", epoch_ade, step=epoch)
+            tf.summary.scalar("FDE", epoch_fde, step=epoch)
+
         print(f"epoch {epoch+1}: loss {epoch_loss:.4f} | ADE {epoch_ade:.4f} | FDE {epoch_fde:.4f}")
 
         val_loss, val_ade, val_fde = 0.0, 0.0, 0.0
         val_batches = 0
-        
+
         for past, future, valid in val_ds:
             pred = model(past, training=False)
             val_loss += float(mse(future, pred, valid).numpy())
@@ -73,6 +91,11 @@ def train_model(model_to_train, BATCH_SIZE=64, epochs=10):
         val_loss /= val_batches
         val_ade  /= val_batches
         val_fde  /= val_batches
+
+        with val_writer.as_default():
+            tf.summary.scalar("loss", val_loss, step=epoch)
+            tf.summary.scalar("ADE", val_ade, step=epoch)
+            tf.summary.scalar("FDE", val_fde, step=epoch)
 
         print(f"Val: loss {val_loss:.4f} | ADE {val_ade:.4f} | FDE {val_fde:.4f}")
 
